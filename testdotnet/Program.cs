486608c7213b3e12d9f0cbce9fcdfcc7a4014669
diego <dego_vila@yahoo.com>
2021-02-07 23:00:55 -0400

    initial commit for dotnet

---
+++ testdotnet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace testdotnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
