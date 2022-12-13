25db8125f07537de5611e6f20e5dd819d11897cd
ruahman <dego_vila@yahoo.com>
2022-12-13 19:41:41 -0400

    csharp_tut

---
+++ csharp_tut/csharp_tut/Variables.cs
@@ -0,0 +1,7 @@
+namespace variables;
+
+public class Variables {
+  public static void Demo() {
+    Console.WriteLine("--- variables ---");
+  }
+}
---
+++ csharp_tut/test_csharp_tut/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace test_csharp_tut;
+using variables;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+      Variables.Demo();
+    }
+}
