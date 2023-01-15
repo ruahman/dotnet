f26d2f8969a33c8310ff6fd7811e641deb6c4cb0
Diego Vila <diego@velascommerce.com>
2023-01-14 23:49:20 -0400

    started on CSharp tutorials

---
+++ dotnet_tuts/dotnet_tuts/CSharpTut/Variables.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTut
+{
+    public class Variables
+    {
+        public static (string, string, int, string, bool, char, int) Strings()
+        {
+            string myName = "Diego";
+
+            string interpolation = $"my name is {myName}";
+
+            return (
+                myName,
+                interpolation,
+                interpolation.Length,
+                myName.ToUpper(),
+                interpolation.Contains("Diego"),
+                myName[0],
+                interpolation.IndexOf("Diego")
+            );
+        }
+
+        public static char Chars()
+        {
+            char mychar = 'A';
+
+            return mychar;
+        }
+
+        public static (int, int) Integers()
+        {
+            int myAge = 42;
+            int abs = Math.Abs(-7);
+            
+
+            return (myAge, abs);
+        }
+
+        public static (float, double, decimal, double) Floats()
+        {
+            float x = 3.12f; // least accurate
+            double y = 3.33333; // default
+            decimal z = 4.566666666m;  // most acurate
+            var pow = Math.Pow(3, 3);
+
+            return (x, y, z, pow);
+        }
+
+        public static bool Boolean()
+        {
+            bool x = true;
+            return x;
+        }
+    }
+}
---
+++ dotnet_tuts/dotnet_tuts/UnitTests/VariableTests.cs
@@ -0,0 +1,52 @@
+﻿using CSharpTut;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+
+namespace UnitTests
+{
+    public class VariableTests
+    {
+        ITestOutputHelper output;
+
+        public VariableTests(ITestOutputHelper output) => this.output = output;
+        
+        [Fact]
+        [Trait("Category","Variables")]
+        public void Strings()
+        {
+            (String first, String second, int length, string upper, bool contains, char myChar, int idx) = Variables.Strings();
+            Assert.Equal("Diego", first);
+            Assert.Equal("my name is Diego", second);
+            Assert.Equal(16, length);
+            Assert.Equal("DIEGO", upper);
+            Assert.True(contains);
+            Assert.Equal('D', myChar);
+            Assert.Equal(11, idx);
+            output.WriteLine(second);
+
+        }
+
+        [Fact]
+        [Trait("Category", "Variables")]
+        public void Chars()
+        {
+            var res = Variables.Chars();
+            Assert.Equal('A', res);
+        }
+
+        [Fact]
+        [Trait("Category", "Variables")]
+        public void Integers()
+        {
+            (int age, int abs) = Variables.Integers();
+            Assert.Equal(42, age);
+            Assert.Equal(7, abs);
+        }
+
+
+    }
+}
