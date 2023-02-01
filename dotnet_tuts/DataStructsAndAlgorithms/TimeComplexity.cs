6de59a73ace5a5bc2be8996cc18d8a399f8d338b
Diego <diego@velascommerce.com>
2023-01-31 21:07:49 -0400

    added time complexity examples

--- dotnet_tuts/DataStructsAndAlgorithms/TimeComplexity.cs
+++ dotnet_tuts/DataStructsAndAlgorithms/TimeComplexity.cs
@@ -2,6 +2,7 @@
 {
     public class TimeComplexity
     {
+        // this is O(n)
         public static int Fun1(int n)
         {
             int m = 0;
@@ -9,6 +10,54 @@
             {
                 m += 1;
             }
+            return m;
+        }
+
+        // this is O(n^2)
+        public static int Fun2(int n)
+        {
+            int m = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    m += 1;
+                }
+            }
+
+            return m;
+        }
+
+        // this is O(n^3)
+        public static int Fun3(int n)
+        {
+            int m = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    for (int k = 0; k < n; k++)
+                    {
+                        m += 1;
+                    }
+                }
+            }
+
+            return m;
+        }
+
+        // this O(log(n)) 
+        public static int Fun4(int n)
+        {
+            int i = 1, m = 0;
+            while (i < n)
+            {
+                m += 1;
+                i = i * 2;
+            }
+            // 2*m = n,
+            // m = log(n)
+
             return m;
         }
     }
--- dotnet_tuts/UnitTests/TimeComplexityTests.cs
+++ dotnet_tuts/UnitTests/TimeComplexityTests.cs
@@ -21,8 +21,32 @@ namespace UnitTests
         [Trait("DataStructsAndAlgorithms", "TimeComplexity")]
         public void Fun1Test()
         {
-            var res = TimeComplexity.Fun1(8);
-            Assert.Equal(8, res);
+            var res = TimeComplexity.Fun1(10);
+            Assert.Equal(10, res);
+        }
+
+        [Fact]
+        [Trait("DataStructsAndAlgorithms", "TimeComplexity")]
+        public void Fun2Test()
+        {
+            var res = TimeComplexity.Fun2(10);
+            Assert.Equal(100, res);
+        }
+
+        [Fact]
+        [Trait("DataStructsAndAlgorithms", "TimeComplexity")]
+        public void Fun3Test()
+        {
+            var res = TimeComplexity.Fun3(10);
+            Assert.Equal(1000, res);
+        }
+
+        [Fact]
+        [Trait("DataStructsAndAlgorithms", "TimeComplexity")]
+        public void Fun4Test()
+        {
+            var res = TimeComplexity.Fun4(16);
+            Assert.Equal(4, res);
         }
     }
 }
