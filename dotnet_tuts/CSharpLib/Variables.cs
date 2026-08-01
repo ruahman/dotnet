0732a37f447834b2d7616e3d2b2ae209543b0b5d
dego_ <dego_vila@yahoo.com>
2026-08-01 08:55:10 -0400

    update

--- dotnet_tuts/CSharpLib/Variables.cs
+++ dotnet_tuts/CSharpLib/Variables.cs
@@ -1,117 +1,119 @@
-﻿namespace CSharpLib
+﻿namespace CSharpLib;
+
+public abstract class Variables
 {
-    public abstract class Variables
+    public static (string, string, int, string, bool, char, int, string) Strings()
     {
-        public static (string, string, int, string, bool, char, int, string) Strings()
-        {
-            const string myName = "Diego";
+        const string myName = "Diego";
 
-            const string interpolation = $"my name is {myName}";
+        const string interpolation = $"my name is {myName}";
 
-            const string literal = @"G:\My Drive\Documents\denote";
-           
-            // this was mapped in UnitTester
-            Console.WriteLine("dont need output!!!!");
+        const string literal = @"G:\My Drive\Documents\denote";
 
-            return (
-                myName,
-                interpolation,
-                interpolation.Length,
-                myName.ToUpper(),
-                interpolation.Contains("Diego"),
-                myName[0],
-                interpolation.IndexOf("Diego", StringComparison.Ordinal),
-                literal
-            );
-        }
+        // this was mapped in UnitTester
+        Console.WriteLine("dont need output!!!!");
 
-        public static char Chars()
-        {
-            char mychar = 'A';
+        return (
+            myName,
+            interpolation,
+            interpolation.Length,
+            myName.ToUpper(),
+            interpolation.Contains("Diego"),
+            myName[0],
+            interpolation.IndexOf("Diego", StringComparison.Ordinal),
+            literal
+        );
+    }
 
-            return mychar;
-        }
+    public static void Chars()
+    {
+        const char myChar = 'A';
 
-        public static (int, int) Integers()
-        {
-            const int myAge = 42;
-            var abs = Math.Abs(-7);
+        Console.WriteLine("{0:c}", myChar);
+    }
 
-            // if you need to use a keyword as a variable name, you can use the @ symbol
-            const int @int = 43;
-            
-            Console.WriteLine(@int);
+    public static void Integers()
+    {
+        const int myAge = 42;
+        var abs = Math.Abs(-7);
 
+        Console.WriteLine("{0}", myAge);
+        Console.WriteLine("{0}", abs);
 
-            return (myAge, abs);
-        }
+        // if you need to use a keyword as a variable name, you can use the @ symbol
+        const int @int = 43;
 
-        public static (float, double, decimal, double) Floats()
-        {
-            float f = 3.12f; // least accurate
-            double d = 3.33333; // default
-            decimal dec = 4.566666666m;  // most acurate
-            var pow = Math.Pow(3, 3);
+        Console.WriteLine(@int);
+    }
 
-            return (f, d, dec, pow);
-        }
+    public static void Floats()
+    {
+        const float f = 3.12f; // least accurate
+        const double d = 3.33333;
+        const decimal dec = 4.566666666m; // most acurate
+        var pow = Math.Pow(3, 3);
 
-        public static bool Booleans()
-        {
-            bool x = true;
-            return x;
-        }
+        Console.WriteLine("{0:f}", f);
+        Console.WriteLine("{0:f}", d);
+        Console.WriteLine("{0:f}", dec);
+        Console.WriteLine("{0:f}", pow);
+    }
 
-        public static int? Nullable()
-        {
-            int? x = null;
+    public static void Booleans()
+    {
+        var x = true;
+        Console.WriteLine("{0}", x);
+    }
 
-            return x;
-        }
+    public static void Nullable()
+    {
+        int? x = null;
+        Console.WriteLine("null: {0}", x.HasValue ? x : "null");
+    }
 
-        public static int Casting()
-        {
-            double myDouble = 9.78;
-            int myInt = (int)myDouble;    // Manual casting: double to int
+    public static void Casting()
+    {
+        const double myDouble = 9.78;
+        const int myInt = (int)myDouble; // Manual casting: double to int
 
-            Console.WriteLine(myDouble);   // Outputs 9.78
-            Console.WriteLine(myInt);      // Outputs 9
+        Console.WriteLine("{0}", myDouble); // Outputs 9.78
+        Console.WriteLine("{0}", myInt); // Outputs 9
+    }
 
-            return myInt;
+    public static void Conversion()
+    {
+        var myInt = 10;
+        var myDouble = 5.25;
+        var myBool = true;
 
-        }
+        var res1 = Convert.ToString(myInt); // convert int to string
+        var res2 = Convert.ToDouble(myInt); // convert int to double
+        var res3 = Convert.ToInt32(myDouble); // convert double to int
+        var res4 = Convert.ToString(myBool); // convert bool to string
 
-        public static (string, double, int, string) Conversion()
-        {
-            int myInt = 10;
-            double myDouble = 5.25;
-            bool myBool = true;
+        Console.WriteLine("{0}", res1);
+        Console.WriteLine("{0}", res2);
+        Console.WriteLine("{0}", res3);
+        Console.WriteLine("{0}", res4);
+    }
 
-            var res1 = Convert.ToString(myInt);    // convert int to string
-            var res2 = Convert.ToDouble(myInt);    // convert int to double
-            var res3 = Convert.ToInt32(myDouble);  // convert double to int
-            var res4 = Convert.ToString(myBool);   // convert bool to string
+    public static void TryParse()
+    {
+        var number = "128";
 
-            return (res1, res2, res3, res4);
+        var success = int.TryParse(number, out var parseValue);
 
-        }
+        Console.WriteLine("{0}", success);
+    }
 
-        public static (bool, int) TryParse()
-        {
-            string number = "128";
+    // you can use out to return more than on variable from a function
+    public static void OutVariables()
+    {
+        DateTime dt;
+        DateTime.TryParse("2/22/2023", out dt);
+        DateTime.TryParse("2/22/2024", out var dt2);
 
-            bool success = int.TryParse(number, out int parseValue);
-            return (success, parseValue);
-        }
-
-        // you can use out to return more than on variable from a function
-        public static (DateTime, DateTime) OutVariables()
-        {
-            DateTime dt;
-            DateTime.TryParse("2/22/2023", out dt);
-            DateTime.TryParse("2/22/2024", out DateTime dt2);
-
-            return (dt, dt2);
-        }
+        Console.WriteLine("{0}", dt);
+        Console.WriteLine("{0}", dt2);
     }
 }
--- dotnet_tuts/UnitTests/VariableTests.cs
+++ dotnet_tuts/UnitTests/VariableTests.cs
@@ -1,124 +1,88 @@
 ﻿using CSharpLib;
 using Xunit.Abstractions;
 
-namespace UnitTests
+namespace UnitTests;
+
+public class VariableTests
 {
-    public class VariableTests
+    private readonly ITestOutputHelper _output;
+
+    public VariableTests(ITestOutputHelper output)
     {
+        _output = output;
 
-        private readonly ITestOutputHelper _output;
+        // console now outputs to xUnit
+        var converter = new Converter(output);
+        Console.SetOut(converter);
+    }
 
-        public VariableTests(ITestOutputHelper output)
-        {
-            this._output = output;
+    [Fact]
+    public void Strings()
+    {
+        var (first, second, length, upper, contains, myChar, idx, literal) = Variables.Strings();
+        Assert.Equal("Diego", first);
+        Assert.Equal("my name is Diego", second);
+        Assert.Equal(16, length);
+        Assert.Equal("DIEGO", upper);
+        Assert.True(contains);
+        Assert.Equal('D', myChar);
+        Assert.Equal(11, idx);
+        Assert.Equal(@"G:\My Drive\Documents\denote", literal);
+        _output.WriteLine(second);
+    }
 
-            // console now outputs to xUnit
-            var converter = new Converter(output);
-            Console.SetOut(converter);
-        }
+    [Fact]
+    public void Chars()
+    {
+        Variables.Chars();
+    }
 
-        [Fact]
-        public void Strings()
-        {
-            var (first, second, length, upper, contains, myChar, idx, literal) = Variables.Strings();
-            Assert.Equal("Diego", first);
-            Assert.Equal("my name is Diego", second);
-            Assert.Equal(16, length);
-            Assert.Equal("DIEGO", upper);
-            Assert.True(contains);
-            Assert.Equal('D', myChar);
-            Assert.Equal(11, idx);
-            Assert.Equal(@"G:\My Drive\Documents\denote", literal);
-            _output.WriteLine(second);
+    [Fact]
+    public void Integers()
+    {
+        Variables.Integers();
+    }
 
-        }
+    [Fact]
+    public void Floats()
+    {
+        Variables.Floats();
+    }
 
-        [Fact]
-        [Trait("CSharp", "Variables")]
-        public void Chars()
-        {
-            var res = Variables.Chars();
-            Assert.Equal('A', res);
-        }
+    [Fact]
+    public void Booleans()
+    {
+        Variables.Booleans();
+    }
 
-        [Fact]
-        [Trait("CSharp", "Variables")]
-        public void Integers()
-        {
-            (int age, int abs) = Variables.Integers();
-            Assert.Equal(42, age);
-            Assert.Equal(7, abs);
-        }
+    [Fact]
+    public void Nullable()
+    {
+        Variables.Nullable();
+    }
 
-        [Fact]
-        [Trait("CSharp", "Variables")]
-        public void Floats()
-        {
-            (float f, double d, decimal dec, double pow) = Variables.Floats();
-            Assert.Equal(3.12f, f);
-            Assert.Equal(3.33333, d);
-            Assert.Equal(4.566666666m, dec);
-            Assert.Equal(27, pow);
-        }
+    [Fact]
+    public void Casts()
+    {
+        Variables.Casting();
+    }
 
-        [Fact]
-        [Trait("CSharp", "Variables")]
-        public void Booleans()
-        {
-            bool x = Variables.Booleans();
-            Assert.True(x);
-        }
-
-        [Fact]
-        [Trait("CSharp", "Variables")]
-        public void Nullable()
-        {
-            int? x = Variables.Nullable();
-            Assert.Null(x);
-        }
-
-        [Fact]
-        [Trait("CSharp", "Variables")]
-        public void Casts()
-        {
-            int x = Variables.Casting();
-            Assert.Equal(9, x);
-        }
-
-        [Fact]
-        [Trait("CSharp", "Variables")]
-        public void Conversion()
-        {
-            var res = Variables.Conversion();
-            Assert.Equal("10", res.Item1);
-            Assert.Equal(10.0, res.Item2);
-            Assert.Equal(5, res.Item3);
-            Assert.Equal("True", res.Item4);
-        }
+    [Fact]
+    public void Conversion()
+    {
+        Variables.Conversion();
+    }
 
 
-        [Fact]
-        [Trait("CSharp", "Variables")]
-        public void TryParse()
-        {
-            var res = Variables.TryParse();
-            Assert.True(res.Item1);
-            Assert.Equal(128, res.Item2);
-        }
+    [Fact]
+    public void TryParse()
+    {
+        Variables.TryParse();
+    }
 
-        [Fact]
-        [Trait("CSharp", "Variables")]
-        public void OutVariables()
-        {
-            var res = Variables.OutVariables();
-
-            var test1 = new DateTime(2023, 2, 22);
-            Assert.Equal(test1, res.Item1);
-
-            var test2 = new DateTime(2024, 2, 22);
-            Assert.Equal(test2, res.Item2);
-        }
-
-
+    [Fact]
+    public void OutVariables()
+    {
+        Variables.OutVariables();
     }
 }
